9c7fdefc9ba8582038f5640d8b277a7db187bec5
lee-eojin <lejin0906@snu.ac.kr>
2025-10-16 19:57:58 +0900

    feat: add empty string validation
    Return 0 when input string is empty

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    const result = this.calculate(input);
+    Console.print(`결과 : ${result}`);
+  }
+
+  calculate(input) {
+    if (input === "") {
+      return 0;
+    }
+    
+    return 0;
   }
 }
 

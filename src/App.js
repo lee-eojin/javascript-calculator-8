dc88bfc78ec2d8e0ee3e8137ee52e40986c00512
lee-eojin <lejin0906@snu.ac.kr>
2025-10-18 15:58:08 +0900

    refactor: sum method to reduce
    Remove duplicated reduce logic in both delimiter paths

--- src/App.js
+++ src/App.js
@@ -25,16 +25,18 @@ class App {
       if (negativeNumber !== undefined) {
         throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
       }
-      const sum = numbers.reduce((acc, num) => acc + num, 0);
-      return sum;
+      return this.sum(numbers);
     }
     const numbers = input.split(/[,:]/).map(Number);
     const negativeNumber = numbers.find(num => num < 0);
     if (negativeNumber !== undefined) {
       throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
     }
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    return sum;
+    return this.sum(numbers);
+  }
+
+  sum(numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
   }
 }
 export default App;

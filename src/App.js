aa401e2fbed2460d776b408063127af78888d159
lee-eojin <lejin0906@snu.ac.kr>
2025-10-18 17:08:02 +0900

    refactor: extract custom delimiter parsing logic
    - Create parseCustomDelimiter() method to parse custom delimiter input
    - Returns number array from custom delimiter format
    - Simplifies calculate() method logic

--- src/App.js
+++ src/App.js
@@ -15,17 +15,21 @@ class App {
     const PREFIX = "//";
 
     if (input.startsWith(PREFIX)) {
-      const normalizedInput = input.replace(/\\n/g, "\n");
-      const delimiterEndIndex = normalizedInput.indexOf("\n");
-      const customDelimiter = normalizedInput.substring(PREFIX.length, delimiterEndIndex);
-      const numberString = normalizedInput.substring(delimiterEndIndex + 1);
-      const numbers = numberString.split(customDelimiter).map(Number);
+      const numbers = this.parseCustomDelimiter(input);
       this.validateNumbers(numbers);
       return this.sum(numbers);
     }
     const numbers = input.split(/[,:]/).map(Number);
     this.validateNumbers(numbers);
     return this.sum(numbers);
+  }
+
+  parseCustomDelimiter(input) {
+    const normalizedInput = input.replace(/\\n/g, "\n");
+    const delimiterEndIndex = normalizedInput.indexOf("\n");
+    const customDelimiter = normalizedInput.substring(2, delimiterEndIndex);
+    const numberString = normalizedInput.substring(delimiterEndIndex + 1);
+    return numberString.split(customDelimiter).map(Number);
   }
 
   validateNumbers(numbers) {
